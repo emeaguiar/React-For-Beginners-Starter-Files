6a87aceec73b39328f3175f0b296f0ea3320eea4
Mario Aguiar <mario@10up.com>
2018-02-13 12:11:18 -0600

    Add go to store method

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	/* constructor() {
+		super();
+
+		this.goToStore = this.goToStore.bind( this );
+	} */
+
+	goToStore( event ) {
+		event.preventDefault();
+		console.log( 'URL changed' );
+		// First grab the text from the box
+		console.log(this.storeInput.value);
+		// Transition  from / to /store/:storeId
+	}
+	
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={ this.goToStore.bind( this ) }>
 				{/* hello */}
 				<h2>Please Enter a Store</h2>
-				<input type="text" placeholder="Store Name" defaultValue={ getFunName() } />
+				<input type="text" placeholder="Store Name" defaultValue={ getFunName() } ref={ ( input ) => { this.storeInput = input } } />
 				<button type="submit">Visit Store</button>
 			</form>
 		);
