7f3ef892b299a1816bc3a8bdf23049221083075c
Mario Aguiar <mario@10up.com>
2018-02-13 16:57:19 -0600

    Connect with firebase to add data

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,6 +6,8 @@ import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
 
+import base from '../base';
+
 class App extends React.Component {
 	constructor() {
 		super();
@@ -19,6 +21,17 @@ class App extends React.Component {
 			fishes: {},
 			order: {}
 		};
+	}
+
+	componentWillMount() {
+		this.ref = base.syncState( `${ this.props.params.storeId }/fishes`, {
+			context: this,
+			state: 'fishes'
+		} );
+	}
+
+	componentWillUnmount() {
+		base.removeBinding( this.ref );
 	}
 
 	addFish( fish ) {
